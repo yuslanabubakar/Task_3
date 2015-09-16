cac358ff6e9faa0790b08f0a797b27145a9a8c0f
Muhammad Yuslan Abubakar <yuslan.black@gmail.com>
2015-09-16 23:25:12 +0700

    Update main.cpp

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -4,6 +4,28 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    // Inisialisasi Single Linked List
+    address P; //Bikin Pointer P
+    list mainList; // Bikin Variable List Utama
+    mainList.first = NULL; // List Pertama diisi Null
+    Buku dataBuku; // Tipe Bentukan untuk wadah Input Info
+
+    cout << "Single Linked List Program - Kelompok 2"<<endl;
+    cout << "MENU APPLICATION : 1. Insert First"<<endl;
+    cout << "                   2. Insert After"<<endl;
+    cout << "                   3. Insert Last"<<endl;
+    cout << "                   4. Delete First"<<endl;
+    cout << "                   5. Delete Last"<<endl;
+    cout << "                   6. Delete After"<<endl;
+    cout << "                   7. View List"<<endl;
+    cout << "                   8. Search Element"<<endl;
+    cout << endl;
+
+    MenuLabel:
+    cout << "Input Your Menu  : "; char menu;
+    cin>>menu; cin.ignore(); cout << endl;
+    switch (menu) {
+      case '1' : createBuku(&dataBuku); P = createElement(dataBuku); insertFirst(&mainList, P); goto MenuLabel;
+      case '7' : viewElement(&mainList); goto MenuLabel;
     return 0;
 }
