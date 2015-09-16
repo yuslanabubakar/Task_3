a63bb933d51c48878134b79c82c36b7fdd8a73a2
widyawirachantika <wirachantika@gmail.com>
2015-09-16 23:29:25 +0700

    Update Main.cpp

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    case '2' : createBuku(&dataBuku); P = createElement(dataBuku); insertAfter(&mainList, P); goto MenuLabel;
+    case '3' : createBuku(&dataBuku); P = createElement(dataBuku); insertLast(&mainList, P); goto MenuLabel;
     return 0;
 }
