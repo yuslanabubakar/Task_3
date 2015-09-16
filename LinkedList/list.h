7b1ac31b5a3da2588788b23add7e2da9bca6dc91
Muhammad Yuslan Abubakar <yuslan.black@gmail.com>
2015-09-16 23:23:03 +0700

    Update List.h

--- LinkedList/list.h
+++ LinkedList/list.h
@@ -1,6 +1,31 @@
 #ifndef LIST_H_INCLUDED
 #define LIST_H_INCLUDED
 
+using namespace std;
 
+struct Buku{
+    string isbn;
+    string judul;
+    string pengarang;
+    string penerbit;
+    double harga;
+};
+
+typedef struct elmlist *address;
+
+struct elmlist{
+    Buku info;
+    address next;
+};
+
+struct list{
+    address first;
+};
+
+address createElement(Buku addBuku);
+void createBuku(Buku *addBuku);
+int countElement(list *L);
+void viewElement(list *L);
+void insertFirst(list *L, address P);
 
 #endif // LIST_H_INCLUDED
